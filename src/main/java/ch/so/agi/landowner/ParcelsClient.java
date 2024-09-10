fc00189951851de435a8ff4480cfea8daa7033f6
Stefan Ziegler <edi.gonzales@gmail.com>
2024-09-10 11:55:51 +0200

    wip

--- src/main/java/ch/so/agi/landowner/ParcelsClient.java
+++ src/main/java/ch/so/agi/landowner/ParcelsClient.java
@@ -1,13 +1,19 @@
 package ch.so.agi.landowner;
 
+import java.io.IOException;
 import java.io.StringWriter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.SoapHeader;
+import org.springframework.ws.soap.SoapMessage;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
+import org.springframework.xml.transform.StringSource;
 
 import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.BezugInhalt;
 import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.GetParcelsByIdRequestType;
@@ -17,6 +23,9 @@ import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.ObjectFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 
 
 public class ParcelsClient extends WebServiceGatewaySupport {
@@ -44,7 +53,8 @@ public class ParcelsClient extends WebServiceGatewaySupport {
        
         request.setVersion("2.1");
         request.setTransactionId("RAUM-14922-1");
-        request.setBezugInhalt(BezugInhalt.INDEX_MIT_EIGENTUM);
+        //request.setBezugInhalt(BezugInhalt.INDEX_MIT_EIGENTUM);
+        request.setBezugInhalt(BezugInhalt.OEFFENTLICH);
         request.setIncludeHistory(false);
         request.getIds().add(egrid+"::::");  // CH258032700664 // CH633284061534
         
@@ -52,11 +62,35 @@ public class ParcelsClient extends WebServiceGatewaySupport {
         Result result1 = new StreamResult(sw1);
 
         marshaller.marshal(request, result1);
-        //System.out.println("Request: " + sw1.toString());
+        System.out.println("Request: " + sw1.toString());
         
         
         GetParcelsByIdResponse response = (GetParcelsByIdResponse) getWebServiceTemplate()
-                .marshalSendAndReceive("https://terravis.so.ch/gbdbs/gbdbs", request);
+                .marshalSendAndReceive("https://terravis.so.ch/gbdbs/gbdbs-pub", request, new WebServiceMessageCallback() {
+                    @Override
+                    public void doWithMessage(WebServiceMessage message) throws IOException, TransformerException {
+                        try {
+                            SoapHeader soapHeader = ((SoapMessage) message).getSoapHeader();
+                            
+                            System.out.println(soapHeader);
+                            
+                            String finalXMLRequest = """
+    <enq:enquirer xmlns:enq="http://www.bedag.ch/raum/gdbds/Enquirer/1.0">
+      <enq:id>BI</enq:id>
+      <enq:name>Testsystem</enq:name>
+    </enq:enquirer>
+                                    """;
+                            StringSource headerSource = new StringSource(finalXMLRequest);
+                            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                            transformer.transform(headerSource, soapHeader.getResult());
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        
+                    }
+                    
+                });
 
         StringWriter sw2 = new StringWriter();
         Result result2 = new StreamResult(sw2);
